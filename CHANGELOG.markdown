6fd2e42b49b316524d17f153a793ccd61b90306a
Edward Kmett <ekmett@gmail.com>
2013-01-06 20:12:18 -0500

    API change to enable default signatures.

---
+++ CHANGELOG.markdown
@@ -0,0 +1,3 @@
+0.3
+---
+* Restructured so users can take advantage of default signatures
--- src/Prelude/Extras.hs
+++ src/Prelude/Extras.hs
@@ -1,29 +1,38 @@
 {-# LANGUAGE CPP #-}
+#ifdef __GLASGOW_HASKELL__ && __GLASGOW_HASKELL__ > 702
+#define DEFAULT_SIGNATURES
+{-# LANGUAGE DefaultSignatures #-}
+#endif
+
 module Prelude.Extras
   (
   -- * Lifted Prelude classes for kind * -> *
-    Eq1(..)
-  , Ord1(..)
-  , Show1(..), shows1
+    Eq1(..), (/=#)
+  , Ord1(..), (<#), (<=#), (>=#), (>#), max1, min1
+  , Show1(..), show1, shows1
   , Read1(..), read1, reads1
 #ifdef __GLASGOW_HASKELL__
+  , readPrec1            -- :: (Read1 f, Read a) => ReadPrec (f a)
+  , readListPrec1        -- :: (Read1 f, Read a) => ReadPrec [f a]
   , readList1Default     -- :: (Read1 f, Read a) => ReadS [f a]
   , readListPrec1Default -- :: (Read1 f, Read a) => ReadPrec [f a]
 #endif
   , Lift1(..)
   -- * Lifted Prelude classes for kind * -> * -> *
-  , Eq2(..)
-  , Ord2(..)
-  , Show2(..), shows2
+  , Eq2(..), (/=##)
+  , Ord2(..), (<##), (<=##), (>=##), (>##), max2, min2
+  , Show2(..), show2, shows2
   , Read2(..), read2, reads2
 #ifdef __GLASGOW_HASKELL__
-  , readList2Default     -- :: (Read1 f, Read a) => ReadS [f a]
-  , readListPrec2Default -- :: (Read1 f, Read a) => ReadPrec [f a]
+  , readPrec2
+  , readListPrec2
+  , readList2Default
+  , readListPrec2Default
 #endif
   , Lift2(..)
   ) where
 
-import Control.Monad (liftM)
+import Control.Arrow (first)
 import Text.Read
 import qualified Text.ParserCombinators.ReadP as P
 import qualified Text.Read.Lex as L
@@ -33,8 +42,13 @@ infixr 4 ==##, /=##, <##, <=##, >=##, >##
 
 class Eq1 f where
   (==#) :: Eq a => f a -> f a -> Bool
-  (/=#) :: Eq a => f a -> f a -> Bool
-  a /=# b = not (a ==# b)
+#ifdef DEFAULT_SIGNATURES
+  default (==#) :: (Eq (f a), Eq a) => f a -> f a -> Bool
+  (==#) = (==)
+#endif
+
+(/=#) :: (Eq1 f, Eq a) => f a -> f a -> Bool
+a /=# b = not (a ==# b)
 
 instance Eq1 Maybe where
   Just a  ==# Just b  = a == b
@@ -46,36 +60,42 @@ instance Eq a => Eq1 (Either a) where
 
 instance Eq1 [] where
   (==#) = (==)
-  
+
 class Eq2 f where
   (==##) :: (Eq a, Eq b) => f a b -> f a b -> Bool
-  (/=##) :: (Eq a, Eq b) => f a b -> f a b -> Bool
-  a /=## b = not (a ==## b)
+#ifdef DEFAULT_SIGNATURES
+  default (==##) :: (Eq (f a b), Eq a, Eq b) => f a b -> f a b -> Bool
+  (==##) = (==)
+#endif
+
+(/=##) :: (Eq2 f, Eq a, Eq b) => f a b -> f a b -> Bool
+a /=## b = not (a ==## b)
 
 instance Eq2 Either where
   (==##) = (==)
 
-class Eq1 f => Ord1 f where 
+class Eq1 f => Ord1 f where
   compare1 :: Ord a => f a -> f a -> Ordering
-  (<#), (<=#), (>=#), (>#) :: Ord a => f a -> f a -> Bool
-  max1, min1 :: Ord a => f a -> f a -> f a
+#ifdef DEFAULT_SIGNATURES
+  default compare1 :: (Ord (f a), Ord a) => f a -> f a -> Ordering
+  compare1 = compare
+#endif
 
-  compare1 x y
-    | x ==# y   = EQ
-    | x <=# y   = LT
-    | otherwise = GT
 
-  x <=# y = compare1 x y /= GT
-  x <#  y = compare1 x y == LT
-  x >=# y = compare1 x y /= LT
-  x ># y  = compare1 x y == GT
+(<#), (<=#), (>=#), (>#) :: (Ord1 f, Ord a) => f a -> f a -> Bool
+max1, min1 :: (Ord1 f, Ord a) => f a -> f a -> f a
 
-  max1 x y 
-    | x >=# y   = x
-    | otherwise = y
-  min1 x y
-    | x <#  y   = x
-    | otherwise = y
+x <=# y = compare1 x y /= GT
+x <#  y = compare1 x y == LT
+x >=# y = compare1 x y /= LT
+x ># y  = compare1 x y == GT
+
+max1 x y
+  | x >=# y   = x
+  | otherwise = y
+min1 x y
+  | x <#  y   = x
+  | otherwise = y
 
 instance Ord1 Maybe where compare1 = compare
 instance Ord a => Ord1 (Either a) where compare1 = compare
@@ -84,37 +104,47 @@ instance Ord1 [] where compare1 = compare
 -- needs Haskell 2011
 -- instance Ord1 Complex where compare1 = compare
 
-class Eq2 f => Ord2 f where 
+class Eq2 f => Ord2 f where
   compare2 :: (Ord a, Ord b) => f a b -> f a b -> Ordering
-  (<##), (<=##), (>=##), (>##) :: (Ord a, Ord b) => f a b -> f a b -> Bool
-  max2, min2 :: (Ord a, Ord b) => f a b -> f a b -> f a b
+#ifdef DEFAULT_SIGNATURES
+  default compare2 :: (Ord (f a b), Ord a, Ord b) => f a b -> f a b -> Ordering
+  compare2 = compare
+#endif
 
-  compare2 x y
-    | x ==## y  = EQ
-    | x <=## y  = LT
-    | otherwise = GT
 
-  x <=## y = compare2 x y /= GT
-  x <##  y = compare2 x y == LT
-  x >=## y = compare2 x y /= LT
-  x >## y  = compare2 x y == GT
+(<##) :: (Ord2 f, Ord a, Ord b) => f a b -> f a b -> Bool
+x <=## y = compare2 x y /= GT
+(<=##) :: (Ord2 f, Ord a, Ord b) => f a b -> f a b -> Bool
+x <##  y = compare2 x y == LT
+(>=##) :: (Ord2 f, Ord a, Ord b) => f a b -> f a b -> Bool
+x >=## y = compare2 x y /= LT
+(>##) :: (Ord2 f, Ord a, Ord b) => f a b -> f a b -> Bool
+x >## y  = compare2 x y == GT
 
-  max2 x y 
-    | x >=## y  = x
-    | otherwise = y
-  min2 x y
-    | x <## y   = x
-    | otherwise = y
+max2 :: (Ord2 f, Ord a, Ord b) => f a b -> f a b -> f a b
+max2 x y
+  | x >=## y  = x
+  | otherwise = y
+
+min2 :: (Ord2 f, Ord a, Ord b) => f a b -> f a b -> f a b
+min2 x y
+  | x <## y   = x
+  | otherwise = y
 
 instance Ord2 Either where compare2 = compare
 
 class Show1 f where
   showsPrec1 :: Show a => Int -> f a -> ShowS
-  show1      :: Show a => f a -> String
-  showList1  :: Show a => [f a] -> ShowS
-  showsPrec1 _ x s = show1 x ++ s
-  show1 x          = shows1 x ""
-  showList1 ls   s = showList__ shows1 ls s
+#ifdef DEFAULT_SIGNATURES
+  default showsPrec1 :: (Show (f a), Show a) => Int -> f a -> ShowS
+  showsPrec1 = showsPrec
+#endif
+  showList1 :: (Show a) => [f a] -> ShowS
+  showList1 ls s = showList__ shows1 ls s
+
+show1 :: (Show1 f, Show a) => f a -> String
+show1 x = shows1 x ""
+
 
 shows1 :: (Show1 f, Show a) => f a -> ShowS
 shows1 = showsPrec1 0
@@ -126,14 +156,18 @@ instance Show a => Show1 ((,) a) where showsPrec1 = showsPrec
 
 -- instance Show1 Complex
 
-class Show2 f where 
+class Show2 f where
   showsPrec2 :: (Show a, Show b) => Int -> f a b -> ShowS
-  show2      :: (Show a, Show b) => f a b -> String
+#ifdef DEFAULT_SIGNATURES
+  default showsPrec2 :: (Show (f a b), Show a, Show b) => Int -> f a b -> ShowS
+  showsPrec2 = showsPrec
+#endif
   showList2  :: (Show a, Show b) => [f a b] -> ShowS
+  showList2 ls s = showList__ shows2 ls s
 
-  showsPrec2 _ x s = show2 x ++ s
-  show2 x          = shows2 x ""
-  showList2 ls   s = showList__ shows2 ls s
+show2      :: (Show2 f, Show a, Show b) => f a b -> String
+show2 x = shows2 x ""
+
 
 shows2 :: (Show2 f, Show a, Show b) => f a b -> ShowS
 shows2 = showsPrec2 0
@@ -148,20 +182,22 @@ showList__ showx (x:xs) s = '[' : showx x (showl xs)
     showl []     = ']' : s
     showl (y:ys) = ',' : showx y (showl ys)
 
-
 class Read1 f where
   readsPrec1    :: Read a => Int -> ReadS (f a)
-  readList1     :: Read a => ReadS [f a]
+#ifdef DEFAULT_SIGNATURES
+  default readsPrec1 :: (Read (f a), Read a) => Int -> ReadS (f a)
+  readsPrec1 = readsPrec
+#endif
 
-  readsPrec1 = readPrec_to_S readPrec1
-  readList1  = readPrec_to_S (list readPrec1) 0 
+  readList1 :: (Read a) => ReadS [f a]
+  readList1  = readPrec_to_S (list readPrec1) 0
 
 #ifdef __GLASGOW_HASKELL__
-  readPrec1     :: Read a => ReadPrec (f a)
-  readListPrec1 :: Read a => ReadPrec [f a]
+readPrec1     :: (Read1 f, Read a) => ReadPrec (f a)
+readPrec1     = readS_to_Prec readsPrec1
 
-  readPrec1     = readS_to_Prec readsPrec1
-  readListPrec1 = readS_to_Prec (\_ -> readList1)
+readListPrec1 :: (Read1 f, Read a) => ReadPrec [f a]
+readListPrec1 = readS_to_Prec (\_ -> readList1)
 #endif
 
 read1  :: (Read1 f, Read a) => String -> f a
@@ -208,15 +244,19 @@ instance Read a => Read1 ((,) a) where
 
 class Read2 f where
   readsPrec2    :: (Read a, Read b) => Int -> ReadS (f a b)
+#ifdef DEFAULT_SIGNATURES
+  default readsPrec2 :: (Read (f a b), Read a, Read b) => Int -> ReadS (f a b)
+  readsPrec2 = readsPrec
+#endif
   readList2     :: (Read a, Read b) => ReadS [f a b]
-  readsPrec2    = readPrec_to_S readPrec2
-  readList2     = readPrec_to_S (list readPrec2) 0 
+  readList2     = readPrec_to_S (list readPrec2) 0
 
 #ifdef __GLASGOW_HASKELL__
-  readPrec2     :: (Read a, Read b) => ReadPrec (f a b)
-  readListPrec2 :: (Read a, Read b) => ReadPrec [f a b]
-  readPrec2     = readS_to_Prec readsPrec2
-  readListPrec2 = readS_to_Prec (\_ -> readList2)
+readPrec2     :: (Read2 f, Read a, Read b) => ReadPrec (f a b)
+readPrec2     = readS_to_Prec readsPrec2
+
+readListPrec2 :: (Read2 f, Read a, Read b) => ReadPrec [f a b]
+readListPrec2 = readS_to_Prec (\_ -> readList2)
 #endif
 
 instance Read2 (,) where
@@ -280,26 +320,31 @@ newtype Lift1 f a = Lift1 { lower1 :: f a }
 instance Eq1 f   => Eq1 (Lift1 f)   where Lift1 a ==# Lift1 b = a ==# b
 instance Ord1 f  => Ord1 (Lift1 f)  where Lift1 a `compare1` Lift1 b = compare1 a b
 instance Show1 f => Show1 (Lift1 f) where showsPrec1 d (Lift1 a) = showsPrec1 d a
-instance Read1 f => Read1 (Lift1 f) where readPrec1 = liftM Lift1 readPrec1
+instance Read1 f => Read1 (Lift1 f) where
+  readsPrec1 d = map (first Lift1) . readsPrec1 d
 
 instance (Eq1 f, Eq a) => Eq (Lift1 f a)       where Lift1 a == Lift1 b = a ==# b
 instance (Ord1 f, Ord a) => Ord (Lift1 f a)    where Lift1 a `compare` Lift1 b = compare1 a b
 instance (Show1 f, Show a) => Show (Lift1 f a) where showsPrec d (Lift1 a) = showsPrec1 d a
-instance (Read1 f, Read a) => Read (Lift1 f a) where readPrec = liftM Lift1 readPrec1
+instance (Read1 f, Read a) => Read (Lift1 f a) where
+  readsPrec d = map (first Lift1) . readsPrec1 d
 
 newtype Lift2 f a b = Lift2 { lower2 :: f a b }
 
 instance Eq2 f   => Eq2 (Lift2 f)   where Lift2 a ==## Lift2 b = a ==## b
 instance Ord2 f  => Ord2 (Lift2 f)  where Lift2 a `compare2` Lift2 b = compare2 a b
 instance Show2 f => Show2 (Lift2 f) where showsPrec2 d (Lift2 a) = showsPrec2 d a
-instance Read2 f => Read2 (Lift2 f) where readPrec2 = liftM Lift2 readPrec2
+instance Read2 f => Read2 (Lift2 f) where
+  readsPrec2 d = map (first Lift2) . readsPrec2 d
 
 instance (Eq2 f, Eq a)     => Eq1 (Lift2 f a)   where Lift2 a ==# Lift2 b = a ==## b
 instance (Ord2 f, Ord a)   => Ord1 (Lift2 f a)  where Lift2 a `compare1` Lift2 b = compare2 a b
 instance (Show2 f, Show a) => Show1 (Lift2 f a) where showsPrec1 d (Lift2 a) = showsPrec2 d a
-instance (Read2 f, Read a) => Read1 (Lift2 f a) where readPrec1 = liftM Lift2 readPrec2
+instance (Read2 f, Read a) => Read1 (Lift2 f a) where
+  readsPrec1 d = map (first Lift2) . readsPrec2 d
 
 instance (Eq2 f, Eq a, Eq b)       => Eq (Lift2 f a b)   where Lift2 a == Lift2 b = a ==## b
 instance (Ord2 f, Ord a, Ord b)    => Ord (Lift2 f a b)  where Lift2 a `compare` Lift2 b = compare2 a b
 instance (Show2 f, Show a, Show b) => Show (Lift2 f a b) where showsPrec d (Lift2 a) = showsPrec2 d a
-instance (Read2 f, Read a, Read b) => Read (Lift2 f a b) where readPrec = liftM Lift2 readPrec2
+instance (Read2 f, Read a, Read b) => Read (Lift2 f a b) where
+  readsPrec d = map (first Lift2) . readsPrec2 d
